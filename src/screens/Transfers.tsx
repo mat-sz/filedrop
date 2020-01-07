ccfa410cbc162388f1abdf01f316eafb0abfa7e4
Mat Sz <contact@matsz.dev>
2020-01-07 19:04:49 +0100

    added dropzone

--- src/screens/Transfers.tsx
+++ src/screens/Transfers.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import QrCode from 'qrcode.react';
+import Dropzone from 'react-dropzone';
 
 const Transfers: React.FC = () => {
     const { code } = useParams<{ code: string }>();
+
+    const onDrop = useCallback(() => {
+        
+    }, []);
 
     return (
         <div className="screen">
@@ -15,6 +20,15 @@ const Transfers: React.FC = () => {
                 <QrCode value={ window.location.href } />
                 or open this URL on another device: { window.location.href }
             </div>
+            <Dropzone onDrop={onDrop}>
+                {({ getRootProps, getInputProps }) => (
+                <div {...getRootProps()} className="dropzone">
+                    <input {...getInputProps()} accept={'*'} />
+                    <div>To send files, drag and drop them here</div>
+                    <div>or click on this area to open a file selection dialog.</div>
+                </div>
+                )}
+            </Dropzone>
         </div>
     );
 }
