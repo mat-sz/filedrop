f50ba210b015cdb2ed63004818fa438f7c1ae0e4
Mat Sz <contact@matsz.dev>
2020-01-09 18:27:18 +0100

    added a forced redirect to the transfers page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { StateType } from '../reducers';
 
 const Home: React.FC = () => {
     const name = useSelector((state: StateType) => state.name);
+    const history = useHistory();
+
+    useEffect(() => {
+        history.push("/" + name);
+    });
 
     return (
         <div className="section center">
-            <Link to={"/" + name} className="button">Receive files</Link>
+            Loading...
         </div>
     );
 }
