43f750edca3ceb5fdd3465eea2c2012db71853ac
Mat Sz <contact@matsz.dev>
2020-02-08 11:48:20 +0100

    added a copy to clipboard button to url section

--- src/components/QrCodeSection.tsx
+++ src/components/QrCodeSection.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import QrCode from 'qrcode.react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 const QrCodeSection: React.FC<{ href: string }> = ({ href }) => {
+    const [ copied, setCopied ] = useState(false);
+
+    const onCopy = useCallback(() => setCopied(true), [ setCopied ]);
+
     return (
         <div>
             <h2>Invite</h2>
@@ -12,6 +17,14 @@ const QrCodeSection: React.FC<{ href: string }> = ({ href }) => {
                 <pre>
                     { href }
                 </pre>
+                <CopyToClipboard
+                    text={href}
+                    onCopy={onCopy}
+                >
+                    <button>
+                        { copied ? 'Copied' : 'Copy to clipboard' }
+                    </button>
+                </CopyToClipboard>
             </div>
         </div>
     );
