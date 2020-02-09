ba8b98756ddf8eb51bbf396dfa536ce6460f411c
Mat Sz <contact@matsz.dev>
2020-02-09 19:32:35 +0100

    fixed formatting for action creators

--- src/actions/state.tsx
+++ src/actions/state.tsx
@@ -2,41 +2,71 @@ import { ActionModel, ClientModel } from "../types/Models";
 import { ActionType } from "../types/ActionType";
 
 export function setErrorAction(error: string): ActionModel {
-    return { type: ActionType.SET_ERROR, value: error };
+    return {
+        type: ActionType.SET_ERROR,
+        value: error
+    };
 };
 
 export function setConnectedAction(connected: boolean): ActionModel {
-    return { type: ActionType.SET_CONNECTED, value: connected };
+    return {
+        type: ActionType.SET_CONNECTED,
+        value: connected
+    };
 };
 
 export function setRtcConfigurationAction(configuration: RTCConfiguration): ActionModel {
-    return { type: ActionType.SET_RTC_CONFIGURATION, value: configuration };
+    return {
+        type: ActionType.SET_RTC_CONFIGURATION,
+        value: configuration
+    };
 };
 
 export function setNetworkNameAction(name: string): ActionModel {
-    return { type: ActionType.SET_NETWORK_NAME, value: name };
+    return {
+        type: ActionType.SET_NETWORK_NAME,
+        value: name
+    };
 };
 
 export function setSuggestedNameAction(name: string): ActionModel {
-    return { type: ActionType.SET_SUGGESTED_NAME, value: name };
+    return {
+        type: ActionType.SET_SUGGESTED_NAME,
+        value: name
+    };
 };
 
 export function setClientIdAction(id: string): ActionModel {
-    return { type: ActionType.SET_CLIENT_ID, value: id };
+    return {
+        type: ActionType.SET_CLIENT_ID,
+        value: id
+    };
 };
 
 export function setClientColorAction(color: string): ActionModel {
-    return { type: ActionType.SET_CLIENT_COLOR, value: color };
+    return {
+        type: ActionType.SET_CLIENT_COLOR,
+        value: color
+    };
 };
 
 export function setNetworkAction(network: ClientModel[]): ActionModel {
-    return { type: ActionType.SET_NETWORK, value: network };
+    return {
+        type: ActionType.SET_NETWORK,
+        value: network
+    };
 };
 
 export function dismissWelcomeAction(): ActionModel {
-    return { type: ActionType.DISMISS_WELCOME, value: null };
+    return {
+        type: ActionType.DISMISS_WELCOME,
+        value: null
+    };
 };
 
 export function dismissErrorAction(): ActionModel {
-    return { type: ActionType.DISMISS_ERROR, value: null };
+    return {
+        type: ActionType.DISMISS_ERROR,
+        value: null
+    };
 };
