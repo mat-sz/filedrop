e54de6bde7a9bdfba0a6030f1e2d4324a4b658fd
Mat Sz <contact@matsz.dev>
2020-02-25 21:39:20 +0100

    added a mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 import { abuseEmail } from '../config';
 
 const Header: React.FC = () => {
+    const [ toggled, setToggled ] = useState(false);
+
+    const toggle = () => setToggled(toggled => !toggled);
+
     return (
         <header>
+            <button className="toggle" onClick={toggle}>
+                { toggled ? <FaTimes /> : <FaBars /> }
+            </button>
             <nav className="menu">
                 <h1>
                     <Link to="/" className="logo">{ process.env.REACT_APP_TITLE }</Link>
                 </h1>
-                <ul>
+                <ul className={toggled ? '' : 'hidden'}>
                     <li>
                         <Link to="/privacy">privacy policy</Link>
                     </li>
