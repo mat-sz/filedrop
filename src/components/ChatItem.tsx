74e6cff4033b3a3e769f0670b885e3a0be2acad7
Mat Sz <contact@matsz.dev>
2021-01-04 16:20:56 +0100

    added a limit to chat message height

--- src/components/ChatItem.tsx
+++ src/components/ChatItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import ReactTimeago from 'react-timeago';
@@ -12,9 +12,18 @@ export interface ChatItemProps {
 }
 
 const ChatItem: React.FC<ChatItemProps> = ({ item }) => {
+  const [expanded, setExpanded] = useState(false);
+  const messageRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (messageRef.current?.offsetHeight < 50) {
+      setExpanded(true);
+    }
+  }, [setExpanded]);
+
   return (
     <motion.li
-      className="subsection info-grid"
+      className={'subsection info-grid ' + (expanded ? 'chat-expanded' : '')}
       {...animationPropsSlide}
       aria-label="Chat message"
     >
@@ -34,7 +43,14 @@ const ChatItem: React.FC<ChatItemProps> = ({ item }) => {
           </button>
         </CopyToClipboard>
       </div>
-      <div className="chat-message">{item.message}</div>
+      <div className="chat-message" ref={messageRef}>
+        {item.message}
+      </div>
+      {!expanded && (
+        <button className="chat-message-more" onClick={() => setExpanded(true)}>
+          Show more
+        </button>
+      )}
     </motion.li>
   );
 };
