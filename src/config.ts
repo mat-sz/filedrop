d269df933fb3e1a40c4986c3822a8919b86f4f3d
Mat Sz <contact@matsz.dev>
2022-10-06 18:24:59 +0200

    add dynamic parameters to the server url

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,18 @@
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 export const title = process.env.REACT_APP_TITLE || 'filedrop';
-export const wsServer =
-  process.env.REACT_APP_SERVER ||
-  'ws://' + window.location.hostname + ':5000/ws';
+export const wsServer = process.env.REACT_APP_SERVER
+  ? process.env.REACT_APP_SERVER.replace('(hostname)', window.location.hostname)
+      .replace(
+        '(protocol)',
+        window.location.protocol === 'https:' ? 'wss:' : 'ws:'
+      )
+      .replace(
+        '(port)',
+        window.location.port ||
+          (window.location.protocol === 'https:' ? '443' : '80')
+      )
+  : 'ws://' + window.location.hostname + ':5000/ws';
 export const nameCharacterSet = 'CEFGHJKMNPQRTVWXY';
 export const nameLength = 5;
 export const Router: any = process.env.REACT_APP_USE_BROWSER_ROUTER
