edae4c3d7398d5e6ef1673d5a683d219f901c3c4
Rehan Mahmood <rehanone@gmail.com>
2022-10-07 21:57:38 -0400

    Set application title dynamically using docker compose environment variables.

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,13 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import { replaceUrlParameters } from './utils/url';
 
-export const title = process.env.REACT_APP_TITLE || 'filedrop';
-export const wsServer = process.env.REACT_APP_SERVER
-  ? replaceUrlParameters(process.env.REACT_APP_SERVER)
+export const title = window.__RUNTIME_CONFIG__.REACT_APP_TITLE || 'filedrop';
+export const wsServer = window.__RUNTIME_CONFIG__.REACT_APP_SERVER
+  ? replaceUrlParameters(window.__RUNTIME_CONFIG__.REACT_APP_SERVER)
   : 'ws://' + window.location.hostname + ':5000/ws';
 export const nameCharacterSet = 'CEFGHJKMNPQRTVWXY';
 export const nameLength = 5;
-export const Router: any = process.env.REACT_APP_USE_BROWSER_ROUTER
+export const Router: any = window.__RUNTIME_CONFIG__.REACT_APP_USE_BROWSER_ROUTER
   ? BrowserRouter
   : HashRouter;
-export const abuseEmail = process.env.REACT_APP_ABUSE_EMAIL || null;
+export const abuseEmail = window.__RUNTIME_CONFIG__.REACT_APP_ABUSE_EMAIL || null;
