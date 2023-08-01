43e2eab5f7d9ba99b07f7fcefd98c13c50575743
Mat Sz <contact@matsz.dev>
2023-08-01 20:04:34 +0200

    fix frontend env variables

--- web/src/config.ts
+++ web/src/config.ts
@@ -2,9 +2,9 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const wsProtocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
 export const wsServer = `${wsProtocol}${window.location.host}/ws`;
-export const defaultAppName = import.meta.env.REACT_APP_NAME || 'filedrop';
+export const defaultAppName = import.meta.env.VITE_APP_NAME || 'filedrop';
 export const nameCharacterSet = 'CEFGHJKMNPQRTVWXY';
 export const nameLength = 5;
-export const Router: any = import.meta.env.REACT_APP_USE_BROWSER_ROUTER
+export const Router: any = import.meta.env.VITE_APP_USE_BROWSER_ROUTER
   ? BrowserRouter
   : HashRouter;
