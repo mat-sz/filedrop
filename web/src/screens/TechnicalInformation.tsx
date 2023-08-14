fed1df81da21be0c6d42dee409ea48c78e8b6b9f
Mat Sz <contact@matsz.dev>
2023-08-14 18:00:33 +0200

    make technical info view simpler

--- web/src/screens/TechnicalInformation.tsx
+++ web/src/screens/TechnicalInformation.tsx
@@ -1,41 +1,28 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 
-import {
-  isFileReaderSupported,
-  isORTCSupported,
-  isWebRTCSupported,
-  isWebSocketSupported,
-} from '../utils/browser.js';
+import * as browser from '../utils/browser.js';
 import { TextSection } from '../components/TextSection.js';
 import { connection } from '../stores/index.js';
 
 export const TechnicalInformation: React.FC = observer(() => {
   const remoteAddress = connection.remoteAddress;
 
+  const info = {
+    remoteAddress,
+    userAgent: navigator.userAgent,
+    ...browser,
+  };
+
   return (
     <TextSection>
       <h2>Technical Information</h2>
       <ul>
-        <li>
-          <strong>Remote address:</strong> {remoteAddress}
-        </li>
-        <li>
-          <strong>User agent:</strong> {navigator.userAgent}
-        </li>
-        <li>
-          <strong>WebRTC:</strong> {isWebRTCSupported ? 'Yes' : 'No'}
-        </li>
-        <li>
-          <strong>ORTC:</strong> {isORTCSupported ? 'Yes' : 'No'}
-        </li>
-        <li>
-          <strong>WebSockets:</strong> {isWebSocketSupported ? 'Yes' : 'No'}
-        </li>
-        <li>
-          <strong>FileReader API:</strong>{' '}
-          {isFileReaderSupported ? 'Yes' : 'No'}
-        </li>
+        {Object.entries(info).map(([name, value]) => (
+          <li key={name}>
+            <strong>{name}:</strong> {String(value)}
+          </li>
+        ))}
       </ul>
     </TextSection>
   );
